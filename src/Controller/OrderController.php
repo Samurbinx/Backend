89466e797c29eba63105d667bcf5427140e177a9
samurbinx <samurbinx@gmail.com>
2024-11-21 15:44:41 +0100

    modified:   src/Controller/OrderController.php

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -27,9 +27,6 @@ use Stripe\PaymentMethod;
 #[Route('/order')]
 class OrderController extends AbstractController
 {
-
-
-
     // EN ESTE SE INTENTA IMPLEMENTAR LA CONFIRMACIÓN DEL PAGO AQUI
     #[Route('/new', name: 'app_order_new', methods: ['POST'])]
     public function newOrder(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, ArtworkRepository $artworkRepository, CartRepository $cartRepository, CartArtworkRepository $cartArtworkRepository): Response
@@ -52,7 +49,6 @@ class OrderController extends AbstractController
 
         // Definir la clave secreta de Stripe
         Stripe::setApiKey('sk_test_51QL60A01qslkTUyp5Up7izZk9E246evOANRPdqRYBzVvurm7H2tgVcBEK5kM24ZxuQzXjz9wkgWEwoWS5JBMFk2a00zAkNeYbE');
-
 
         try {
             $entityManager->beginTransaction();
@@ -165,101 +161,4 @@ class OrderController extends AbstractController
             return new JsonResponse(['error' => $e->getMessage()], 400);
         }
     }
-
-    // #[Route('/new', name: 'app_order_new', methods: ['POST'])]
-    // public function newOrder(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, ArtworkRepository $artworkRepository, CartRepository $cartRepository, CartArtworkRepository $cartArtworkRepository): Response
-    // {
-
-    //     $data = json_decode($request->getContent(), true);
-
-    //     if (!isset($data['orderData'])) {
-    //         return new JsonResponse(['error' => 'Datos incompletos'], 400);
-    //     }
-
-    //     // Datos del pedido
-    //     $orderData = $data['orderData'];
-
-    //     // Definir la clave secreta de Stripe
-    //     Stripe::setApiKey('sk_test_51QL60A01qslkTUyp5Up7izZk9E246evOANRPdqRYBzVvurm7H2tgVcBEK5kM24ZxuQzXjz9wkgWEwoWS5JBMFk2a00zAkNeYbE');
-
-
-    //     try {
-    //         $entityManager->beginTransaction();
-
-    //         $order = new Order();
-
-    //         $user = $userRepository->find($orderData['user_id']);
-    //         if (!$user) {
-    //             return new JsonResponse(['error' => 'Usuario no encontrado'], 404);
-    //         }
-    //         $cart = $cartRepository->find($orderData['cart_id']);
-    //         if (!$cart) {
-    //             return new JsonResponse(['error' => 'Carrito no encontrado'], 404);
-    //         }
-
-    //         $total_amount = $orderData['total_amount'];
-    //         if (!$total_amount) {
-    //             return new JsonResponse(['error' => 'Precio total no encontrado'], 404);
-    //         }
-
-    //         $order->setUser($user);
-    //         $order->setTotalAmount($total_amount);
-    //         $entityManager->persist($order);
-    //         $entityManager->flush();
-
-
-    //         // Artworks asociados al carrito actual
-    //         $cartArtworks = $cart->getCartArtworks();
-    //         foreach ($cartArtworks as $cartArtwork) {
-    //             if ($cartArtwork->isSelected()) {
-    //                 $entityManager->remove($cartArtwork);
-    //             }
-    //         }
-    //         $entityManager->persist($cart);
-    //         $entityManager->flush();
-
-    //         // Por cada obra que se está comprando
-    //         $artworks = $orderData['artworks'];
-    //         foreach ($artworks as $artwork_id) {
-    //             $artwork = $artworkRepository->find($artwork_id);
-    //             if (!$artwork) {
-    //                 return new JsonResponse(['error' => 'Obra de arte no encontrada'], 404);
-    //             }
-    //             if ($artwork->isSold()) {
-    //                 return new JsonResponse(['error' => 'La obra que está intentando comprar ya está vendida'], 404);
-    //             }
-    //             if (!$artwork->isSold()) {
-    //                 $artwork->setSold(true);
-    //                 $artwork->setOrderId($order);
-
-    //                 // Todos los carritos de todos los usuarios en los que está esta obra
-    //                 $allcarts = $artwork->getCartArtworks();
-    //                 foreach ($allcarts as $cartArtwork) {
-    //                     $cartArtwork->setSelected(false);
-    //                     $entityManager->persist($cartArtwork);
-    //                 }
-
-    //                 $entityManager->persist($artwork);
-    //                 $entityManager->flush();
-    //             }
-    //         }
-
-
-    //         $entityManager->persist($order);
-    //         $entityManager->flush();
-
-    //         $entityManager->commit();
-    //         return new JsonResponse([
-    //             'message' => 'Se ha completado el pedido',
-    //             'order' => $order,
-    //             'artworks' => $order->getArtworks()
-    //         ], 200);
-    //     } catch (\Exception $e) {
-    //         $entityManager->rollback();
-
-    //         // Retornar el error
-    //         return new JsonResponse(['error' => $e->getMessage()], 400);
-    //     }
-    // }
-
 }
